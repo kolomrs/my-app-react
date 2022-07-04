15a038721d9e5d39441cd860c12b12487b3f93ce
Roman Kolomycev <reveillian@mail.ru>
2022-07-04 13:05:47 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import reactDOM from 'react-dom';
+import "bootstrap/dist/css/bootstrap.css"
+
+const element = <h1>Hello World</h1>;
+
+reactDOM.render(element, document.getElementById("root"));
